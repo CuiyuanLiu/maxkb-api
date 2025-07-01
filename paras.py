38ecc9b3607104754411d70517d6d84097b9068e
並不文藝的小朋友 <34060865+CuiyuanLiu@users.noreply.github.com>
2025-07-01 19:12:44 +0800

    Create paras.py

---
+++ paras.py
@@ -0,0 +1,6 @@
+# Modify your parameter here.
+class MaxKBParas():
+    input_url="http://127.0.0.1:8080/api/application"
+    application_id="Your-Application-ID=Here"
+    port=2000
+    model_id="Your Model Name here"
